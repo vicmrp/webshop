e64f1e0c001b508e731bdc9b27b6f3d5829e4ce4
Victor Reipur <35473080+vicmrp@users.noreply.github.com>
2021-09-16 21:30:17 +0200

    Update authorize-payment.php

--- public/quickpay/authorize-payment.php
+++ public/quickpay/authorize-payment.php
@@ -1,6 +1,6 @@
 <?php
 
-shell_exec(`curl -u ':023a7893ff8c4f0159fb9c43dce4178c90cce8833267cdd0b60ba8ea23b03003' -H 'content-type:application/json' -H 'Accept-Version:v10' -X PUT -d '{"amount":1000}' https://api.quickpay.net/payments/99685196/link`);
+shell_exec(`curl -u ':apiKey' -H 'content-type:application/json' -H 'Accept-Version:v10' -X PUT -d '{"amount":1000}' https://api.quickpay.net/payments/99685196/link`);
 
 
 ?>
