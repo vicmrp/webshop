42ef4eb52c5602be924f5c7e5915185d1115db76
Victor Reipur <victor.reipur@gmail.com>
2021-09-19 16:21:28 +0200

    renset callback.php

--- public/quickpay/callback.php
+++ public/quickpay/callback.php
@@ -1,23 +1,29 @@
 <?php
-$privateKey = file_get_contents('../../secret/quickpay_privatkey');
+
+
+// Dokumentation - https://learn.quickpay.net/tech-talk/api/callback/#callback
+// Denne funktions inputs kommer fra quickpay nr en kunde har betalt
+
+
 
 function sign($base, $private_key) {
   return hash_hmac("sha256", $base, $private_key);
 }
 
 $request_body = file_get_contents("php://input");
+$privateKey = file_get_contents('../../secret/quickpay_privatkey');
 $checksum     = sign($request_body, $privateKey);
 
+
 if ($checksum == $_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"]) {
-
+  // Handter betaling
+  
   // Hvis betalingen er gennemført sa gem ordre og lokation samt send email til køber
-
+  
   // Request is authenticated
 
-  // Hent info om varen ordren og put den i en fil kaldet varenummeret, herefter vedhæft filen til mailen
-  
-  // echo $request_body;
-  file_put_contents("../../secret/flow_example/quickpay_callback.json", $request_body);
+  // Hent info om varerordren og put den i en fil kaldet varenummeret, herefter vedhæft filen til mailen.
+
 }
 
 
