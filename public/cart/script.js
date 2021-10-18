c9159f00043665e4730e162046636389aeb0cdb7
Julian Genevey Jensen <just-julian@hotmail.com>
2021-10-18 20:26:02 +0200

    Global commit

---
+++ public/cart/script.js
@@ -0,0 +1,89 @@
+// Hvad skal den indenholde 
+
+
+let y = ` {
+    "customer": {
+        "fullname": null,
+        "address": {
+            "street": null,
+            "postal_code": null,
+            "city": null
+        },
+        "contact": {
+            "phone": null,
+            "email": null
+        },
+        "company": {
+            "cvr_number": null,
+            "company_name": null
+        }
+    },
+    "order": {
+        "order_id": 3598943,
+        "order_status": {
+            "payment": {
+                "accepted": false,
+                "currency": "DKK",
+                "amount": 31760
+            },
+            "email": {
+                "confirmation_sent": false,
+                "invoice_sent": false
+            }
+        },
+        "order_item": [
+            {
+                "product_name": "cat6 UTP Dataudtag RJ45 1-stik - Hvid",
+                "product_id": "77632",
+                "price": 2320,
+                "quantity": 6
+            },
+            {
+                "product_name": "cat 5e U\/UTP Netv\u00e6rkskabel samler.",
+                "product_id": "CCGP89005WT",
+                "price": 960,
+                "quantity": 4
+            },
+            {
+                "product_name": "kabelsamler",
+                "product_id": "2312314",
+                "price": 1000,
+                "quantity": 14
+            }
+        ]
+    },
+    "shipment": {
+        "tracking_number": null,
+        "order_collected": false,
+        "address": {
+            "street_name": null,
+            "street_number": null,
+            "postal_code": null,
+            "city": null
+        }
+    }
+}
+`;
+
+
+
+
+
+
+let cart  = {
+    item1: 1,
+    item2: 2,
+}
+
+
+let x = JSON.stringify(cart)
+
+// Saves permanent 
+localStorage.setItem('myCat', x);
+localStorage.setItem('cart', y);
+
+
+const cat = localStorage.getItem('myCat');
+
+
+console.log(cat)
--- public/interfaces/ISession.php
+++ public/interfaces/ISession.php
@@ -5,9 +5,9 @@ require __DIR__.'/../global-requirements.php';
 
 interface ISession {
 
-  public function create(int $x);
+  public function create(); // Creater ligesa snart en bruger tilgar siden.
 
-  public function get_by_id($order_id);
+  public function get_session_by_id($order_id); // Henter data om tidligere session baseret pa session id
 
   public function update_by_id($order_id);
 
