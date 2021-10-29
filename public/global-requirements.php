861d3407253ee0a4af8fdd52153c1264a50f1a7a
Victor Reipur <victor.reipur@gmail.com>
2021-10-29 14:27:48 +0200

    Automatisk indhentning af klasser og interfaces

--- public/global-requirements.php
+++ public/global-requirements.php
@@ -20,21 +20,26 @@ require_once 'library.php';
 
 
 // ----- namespaces - inkludere alle klasserne ----- //
-require_once _from_top_folder().'/classes/session/ISession.php';
-require_once _from_top_folder().'/classes/session/Session.php';
-require_once _from_top_folder().'/classes/db_conn/Db_Conn.php';
-require_once _from_top_folder().'/classes/mail/Mail.php';
-require_once _from_top_folder().'/classes/api/quickpay/Quickpay.php';
-require_once _from_top_folder().'/classes/api/postnord/Postnord.php';
-require_once _from_top_folder().'/classes/api/dawa/Dawa.php';
-require_once _from_top_folder().'/classes/session/customer/address/Address.php';
-require_once _from_top_folder().'/classes/session/customer/company/Company.php';
-require_once _from_top_folder().'/classes/session/customer/contact/Contact.php';
-require_once _from_top_folder().'/classes/session/customer/Customer.php';
-require_once _from_top_folder().'/classes/session/shipment/address/Address.php';
-require_once _from_top_folder().'/classes/session/shipment/Shipment.php';
-require_once _from_top_folder().'/classes/session/order/Order.php';
-require_once _from_top_folder().'/classes/session/order/order_item/Order_Item.php';
-require_once _from_top_folder().'/classes/session/order/order_status/email/Email.php';
-require_once _from_top_folder().'/classes/session/order/order_status/Order_Status.php';
-require_once _from_top_folder().'/classes/session/order/order_status/payment/Payment.php';
+// require_once _from_top_folder().'/classes/session/ISession.php';
+// require_once _from_top_folder().'/classes/session/Session.php';
+// require_once _from_top_folder().'/classes/db_conn/Db_Conn.php';
+// require_once _from_top_folder().'/classes/mail/Mail.php';
+// require_once _from_top_folder().'/classes/api/quickpay/Quickpay.php';
+// require_once _from_top_folder().'/classes/api/postnord/Postnord.php';
+// require_once _from_top_folder().'/classes/api/dawa/Dawa.php';
+// require_once _from_top_folder().'/classes/session/customer/address/Address.php';
+// require_once _from_top_folder().'/classes/session/customer/company/Company.php';
+// require_once _from_top_folder().'/classes/session/customer/contact/Contact.php';
+// require_once _from_top_folder().'/classes/session/customer/Customer.php';
+// require_once _from_top_folder().'/classes/session/shipment/address/Address.php';
+// require_once _from_top_folder().'/classes/session/shipment/Shipment.php';
+// require_once _from_top_folder().'/classes/session/order/Order.php';
+// require_once _from_top_folder().'/classes/session/order/order_item/Order_Item.php';
+// require_once _from_top_folder().'/classes/session/order/order_status/email/Email.php';
+// require_once _from_top_folder().'/classes/session/order/order_status/Order_Status.php';
+// require_once _from_top_folder().'/classes/session/order/order_status/payment/Payment.php';
+
+$di = new RecursiveDirectoryIterator(_from_top_folder().'/classes');
+foreach (new RecursiveIteratorIterator($di) as $filename => $file) { 
+  if (!is_dir($filename)) require_once $filename;  
+}
