88460c13555fb655e3cb8b2ebeb1516944b6aa69
Victor Reipur <victor.reipur@gmail.com>
2021-11-28 21:51:59 +0100

    flytter globale js ændringer

--- public/login/main.js
+++ public/login/main.js
@@ -1,9 +1,5 @@
 import Login from '../_global/js/classes/login.js'
 
-String.prototype.replaceAt = function(index, replacement) {
-  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-}
-
 const login = new Login(
   document.getElementById('_main-login_username'),
   document.getElementById('_main-login_password'),
