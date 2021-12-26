59253525b281086fc56c3bb41c9255298eebff61
Victor Reipur <victor.reipur@gmail.com>
2021-12-26 13:25:46 +0100

    basket_storage

--- public/_global/js/global.js
+++ public/_global/js/global.js
@@ -1,11 +1,20 @@
 import Header from './classes/header.js'
+import Basket from './classes/basket.js'
 
 const global = {
   header: function () {
     const logoutBtn = document.getElementById('_header_logout')
     const header = new Header(logoutBtn)
+  },
+
+  basket: () => {
+    const basket = new Basket()
+    basket.addItem("cat6 UTP Dataudtag RJ45 1-stik - Hvid", "77632", 2320, 6)
+    // basket.addItem("cat 5e U/UTP Netværkskabel samler.", "CCGP89005WT", 960, 4)
   }
+  
 }
 
 global.header()
+global.basket()
 
