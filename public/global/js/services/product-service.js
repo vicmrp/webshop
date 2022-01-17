bea96976c35a300b4e367852308e4c88529ccf38
Victor Reipur <victor.reipur@gmail.com>
2022-01-17 11:16:19 +0100

    gui

--- public/global/js/services/product-service.js
+++ public/global/js/services/product-service.js
@@ -1,9 +1,6 @@
 import Interface from "../classes/interface.js";
 
 export default class ProductService extends Interface {
-  hello() {
-    console.log("Hello World from product service")
-  }
 
   async getAllProducts() {
     const functioncall = 'get_all_products'
@@ -21,6 +18,57 @@ export default class ProductService extends Interface {
     return serverResponse
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  presentProducts(divtarget, products) {
+
+    const productDiv = document.getElementById(divtarget)
+
+    products.forEach(element => {
+      console.log(element);
+      
+      const productPill = document.createElement("div")
+      productPill.setAttribute("id", element.id)
+
+      const img = document.createElement("img")
+      img.src = `/img/${element.id}.png`
+      productPill.appendChild(img)
+
+      const name = document.createElement("h1")
+      name.innerHTML = element.name
+      productPill.appendChild(name)
+
+
+      const price = document.createElement("p")
+      const stringPrice = element.price.toString()
+      const kroner = stringPrice.substr(0,3)
+      const ore = stringPrice.substr(-2)
+      price.innerHTML = kroner.concat(",", ore, " ", "kr.")
+      productPill.appendChild(price)
+
+      const buyBtn = document.createElement("a")
+      buyBtn.setAttribute("id", `buyBtn.${element.id}`)
+            
+      
+      productDiv.appendChild(productPill)
+    });
+
+  }
 
 
 
--- public/home/main.js
+++ public/home/main.js
@@ -1,8 +1,19 @@
 import ProductService from '../global/js/services/product-service.js'
 
-console.log("Hello World");
 
 const productService = new ProductService()
 
-productService.hello()
-productService.getAllProducts().then( object => {console.log(object);})
+productService.getAllProducts().then( products => {
+  
+  // console.log(products);
+  productService.presentProducts("product-div", products.list_of_products)
+
+
+})
+
+// const productPresentation1 = document.querySelector("steen_karriere")
+// const productPresentation2 = document.querySelector("steen_roman")
+
+const div1 = document.getElementById("1")
+
+
