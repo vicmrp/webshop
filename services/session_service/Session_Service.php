cac9415524e966e21df56d4e7a3cfdd6930be6a1
Victor Reipur <vicre@byg.dtu.dk>
2022-04-24 20:19:54 +0200

    indsætter session til database

--- services/session_service/Session_Service.php
+++ services/session_service/Session_Service.php
@@ -3,20 +3,22 @@
 namespace vezit\services\session_service;
 
 
+use vezit\dto\session\Session;
 use vezit\dto\session\response\Session_Response;
-use vezit\services\product_service\Product_Service;
-use vezit\dto\class\session\Session;
-use vezit\dto\class\session\customer\Customer as Customer;
+use vezit\dto\class\session\customer\Customer;
 use vezit\dto\class\session\customer\address\Address as Customer_Address;
 use vezit\dto\class\session\customer\company\Company as Customer_Company;
 use vezit\dto\class\session\customer\contact\Contact as Customer_Contact;
 use vezit\dto\class\session\order\Order;
-use vezit\dto\class\session\order\order_item\Order_Item;
-use vezit\dto\class\session\order\order_status\Order_Status;
-use vezit\dto\class\session\order\order_status\email\Email;
-use vezit\dto\class\session\order\order_status\payment\Payment as Order_Payment;
+use vezit\dto\class\session\order\item\Item;
+use vezit\dto\class\session\order\status\Status;
+use vezit\dto\class\session\order\status\email\Email;
+use vezit\dto\class\session\order\status\payment\Payment;
 use vezit\dto\class\session\shipment\Shipment;
 use vezit\dto\class\session\shipment\address\Address as Shipment_Address;
+use vezit\repositories\session_repository\Session_Repository;
+use vezit\services\product_service\Product_Service;
+use vezit\entities\session\Session_Entity;
 
 require __DIR__ . '/../../global-requirements.php';
 
@@ -56,7 +58,7 @@ class Session_Service
     }
 
 
-    public function set_customer() : Session_Response
+    public function set_customer(Customer $cu) : Session_Response
     {
         $session_response = $this->get_session();
 
@@ -67,6 +69,48 @@ class Session_Service
         $_SESSION["session_response"] = serialize($session_response);
 
         return $this->get_session();
+    }
+
+
+    public function insert_session_to_database() : bool
+    {
+
+        $session_response = $this->get_session();
+
+        $session_repository = new Session_Repository;
+        $session_entity = new Session_Entity(
+            $session_response->session->session_pk = null,
+            $session_response->session->session_id,
+            $session_response->session->customer->fullname,
+            $session_response->session->customer->details_satisfied_for_payment,
+            $session_response->session->customer->address->street,
+            $session_response->session->customer->address->postal_code,
+            $session_response->session->customer->address->city,
+            $session_response->session->customer->address->country,
+            $session_response->session->customer->address->phone,
+            $session_response->session->customer->address->email,
+            $session_response->session->customer->company->cvr_number,
+            $session_response->session->customer->company->name,
+            $session_response->session->order->order_id,
+            $session_response->session->order->item,
+            $session_response->session->order->status->payment->accepted,
+            $session_response->session->order->status->payment->currency,
+            $session_response->session->order->status->payment->amount,
+            $session_response->session->order->status->payment->quickpay_id,
+            $session_response->session->order->status->payment->details_satisfied,
+            $session_response->session->order->status->email->confirmation_sent,
+            $session_response->session->order->status->email->invoice_sent_to_customer,
+            $session_response->session->shipment->tracking_number,
+            $session_response->session->shipment->order_collected,
+            $session_response->session->shipment->details_satisfied,
+            $session_response->session->shipment->address->street_name,
+            $session_response->session->shipment->address->street_number,
+            $session_response->session->shipment->address->postal_code,
+            $session_response->session->shipment->address->city
+        );
+
+
+        return $session_repository->insert($session_entity);
     }
 
 }
