e84a0ed8b43bbda8e0b5da9c0c8d759e68586301
Victor Reipur <vicre@byg.dtu.dk>
2022-05-15 15:23:17 +0200

    mocking forstået

--- tests/User_Repository_Test.php
+++ tests/User_Repository_Test.php
@@ -60,4 +60,42 @@ class User_Repository_Test extends TestCase
         $this->assertEquals($users[2]->email, 'viggo_mortensen@gmail.com');
 
     }
+
+    /** @test */
+    public function get_all__shall_return_array_of_user_entities_using_mocking_on_super_repository() {
+        // Arrange
+        $dt = new \DateTime;
+        $mock_users = new Users;
+        $mock_user_1 =  new User(1, $dt, $dt, 'victor.reipur@gmail.com', 'Passw0rd');
+        $mock_user_2 =  new User(1, $dt, $dt, 'viggo_mortensen@gmail.com', 'Passw0rd');
+        $mock_users->set([1 => $mock_user_1, 2 => $mock_user_2]);
+        $mock_super_repository = $this->createMock(Super_Repository::class);
+
+        $associative_array = [0 =>
+        ['user_pk' => 2,
+         'datetime_created' => "2022-05-15 03:30:50",
+         'datetime_last_modified' => "2022-05-15 03:30:50",
+         'email' => "steengede@gmail.com",
+         'hashed_password' => "password"
+        ]];
+
+        $mock_super_repository->method('get_all')
+            ->willReturn($associative_array);
+
+        $user_repository = new User_Repository($mock_super_repository);
+
+
+        $users = $user_repository->get_all();
+
+
+        // Assert
+        foreach ($users->get() as $user) {
+            if (!($user instanceof User)) {
+                $this->fail('User_Repository::get_all() shall return an array of User objects');
+            }
+        }
+
+        $this->assertInstanceOf(Users::class, $users);
+        $this->assertEquals("steengede@gmail.com", $users->get()[2]->email);
+    }
 }
