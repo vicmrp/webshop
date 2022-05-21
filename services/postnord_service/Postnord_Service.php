b0fd34f85af18a24397a37eccd670646cdfb2c3e
Victor Reipur <vicre@byg.dtu.dk>
2022-05-21 22:58:16 +0200

    no idea what i did

--- services/postnord_service/Postnord_Service.php
+++ services/postnord_service/Postnord_Service.php
@@ -2,19 +2,13 @@
 
 namespace vezit\services\postnord_service;
 
-use vezit\classes\session as Session;
-use vezit\dto\session\response as Session_Response;
-use vezit\classes\session\order\order_item as Order_Item;
-use vezit\services\product_service as Product_Service;
-use vezit\classes\error as Error;
 
-use vezit\classes\session\shipment as Shipment;
-use vezit\classes\session\shipment\address as Address;
-use vezit\classes\api\dawa as Dawa;
-use vezit\classes\api\postnord as Postnord;
-use vezit\dto\postnord\request as Request;
-use vezit\dto\postnord\response as Response;
-use vezit\services\session_service as Session_Service;
+
+use vezit\classes\api\dawa\Dawa;
+use vezit\classes\api\postnord\Postnord;
+use vezit\dto\postnord\request\Request;
+use vezit\dto\postnord\response\Response;
+use vezit\services\session_service\Session_Service;
 
 require __DIR__.'/../../global-requirements.php';
 
@@ -30,9 +24,9 @@ class Postnord_Service
   {
 
 
-    $sanitized_address_response = Dawa\Dawa::call_get_sanitized_address(
-      (string)$this->session->customer->address->get_street(),
-      (string)$this->session->customer->address->get_postal_code()
+    $sanitized_address = Dawa::call_get_sanitized_address(
+        $street_name_and_house_number = "",
+        $postal_code = ""
     );
 
 
