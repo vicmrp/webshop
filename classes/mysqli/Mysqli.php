382395198b8df17460f60d70350869842dc80887
Victor Reipur <victor.reipur@gmail.com>
2023-02-22 13:35:55 +0000

    Writing comments to make code more sense

--- classes/mysqli/Mysqli.php
+++ classes/mysqli/Mysqli.php
@@ -4,11 +4,11 @@ namespace vezit\classes\mysqli;
 
 class Mysqli
 {
+
+    // ------------ SINGLETON PATTERN ENDS HERE -------------- //
     private $_db_conn = null;
     private static $_times_instantiated = 0;
     private static $_instance = null;
-
-
 
 
     // Return a new instance if not already instantiated
@@ -22,9 +22,27 @@ class Mysqli
     {
         global $g_db_conn;
         $this->_db_conn = ($this->_db_host && $this->_db_user && $this->_db_pass && $this->_db_name) ?
+
+
+            // If all the parameters are passed in, it means that you want to use a different database
+            // This is useful for testing
+            // For example, you can use this part of code to point to another database
+            // Super_Repository::get_instance(
+            //     Mysqli::get_instance(
+            //         'database-service',
+            //         'testuser',
+            //         'Passw0rd',
+            //         $g_db_conn->dbname
+            //     )
+            // )
             new \mysqli($this->_db_host, $this->_db_user, $this->_db_pass, $this->_db_name)         :
+
+            // Otherwise the constructer will just use the global connection
+            // This is useful for production and none unit testing
             new \mysqli($g_db_conn->servername, $g_db_conn->username, $g_db_conn->password, $g_db_conn->dbname);
 
+        // Increment the number of times the class has been instantiated
+        // I made this to see how many times the class has been instantiated, ideally it should only be instantiated once
         self::$_times_instantiated++;
     }
 
@@ -32,4 +50,5 @@ class Mysqli
     {
         return $this->_db_conn;
     }
+    // ------------ SINGLETON PATTERN ENDS HERE -------------- //
 }
